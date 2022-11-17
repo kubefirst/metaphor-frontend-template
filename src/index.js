3c815be20383cf54b849cad210023d7b03683b1d
Cristhian Fernández <CristhianF7@gmail.com>
2022-11-17 18:40:42 -0500

    feat: nextjs migration (#22)
    * feat: nextjs migration
    
    * fix: clean up
    
    * feedback
    
    * fix
    
    * remove platform
    
    Co-authored-by: Cristhian Fernández <cristhianf@Cristhian-k1.local>

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-
-const app = document.getElementById("app");
-const root = ReactDOM.createRoot(app)
-root.render(<App/>)
--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-export const metaphorJsApi = axios.create({
-    baseURL: `https://${window.__env__.METAPHOR_JS_API_BASE_URL}`
-})
-
-export const metaphorGoApi = axios.create({
-    baseURL: `https://${window.__env__.METAPHOR_GO_API_BASE_URL}`
-})
